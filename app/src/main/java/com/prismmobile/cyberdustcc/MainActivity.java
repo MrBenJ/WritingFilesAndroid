db5cd175d0eb677f5e8185a582ff5136e4e7ae0b
Ben Junya <bjunya@gmail.com>
2015-06-18 10:30:06 -0700

    Initial commit

---
+++ app/src/main/java/com/prismmobile/cyberdustcc/MainActivity.java
@@ -0,0 +1,22 @@
+package com.prismmobile.cyberdustcc;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "Starting app...");
+    }
+
+
+
+}
