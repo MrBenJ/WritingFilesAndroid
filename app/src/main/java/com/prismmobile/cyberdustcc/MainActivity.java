8551e0253b35e8e6d328f4eae47c58a4c5c572a8
Ben Junya <bjunya@gmail.com>
2015-06-18 11:41:38 -0700

    Added onClick Listeners to new alertdialog onlongclick menu

--- app/src/main/java/com/prismmobile/cyberdustcc/MainActivity.java
+++ app/src/main/java/com/prismmobile/cyberdustcc/MainActivity.java
@@ -1,5 +1,10 @@
 package com.prismmobile.cyberdustcc;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,25 +25,27 @@ public class MainActivity extends ActionBarActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
-    List<String> noteList = new ArrayList<String>();
+    List<String> noteList = new ArrayList<>();
     EditText textInput;
-    static TextAdapter adapter;
+    TextAdapter adapter;
     ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "Starting app...");
 
+        final ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         Button submitButton = (Button) findViewById(R.id.submit);
         textInput = (EditText) findViewById(R.id.textInput);
         listView = (ListView) findViewById(R.id.listView);
         adapter = new TextAdapter(this, noteList);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, "Submit button clicked");
+
                 noteList.add(textInput.getText().toString());
                 textInput.setText("");
             }
@@ -47,13 +55,36 @@ public class MainActivity extends ActionBarActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                ClipData clip = ClipData.newPlainText(getString(R.string.simple_text), noteList.get(position));
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(MainActivity.this, getString(R.string.text_copied), Toast.LENGTH_LONG).show();
             }
         });
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Pick an option")
+                       .setItems(R.array.options, new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int which) {
+                               switch(which) {
+                                   
+                                   // EDIT
+                                   case 0:
+                                       break;
+                                   // DELETE
+                                   case 1:
+                                       break;
+                                   // SHARE
+                                   case 2:
+                                       break;
+
+                               }
+                           }
+                       })
+                       .create();
                 return false;
             }
         });
